fe238ca73b4752dc51d1ca874b8a4404dbec3833
App Academy Student <student@appacademy.io>
2014-10-07 15:53:16 -0400

    initiali commit; finish phase IV

--- skeleton/lib/phase3/controller_base.rb
+++ skeleton/lib/phase3/controller_base.rb
@@ -7,6 +7,14 @@ module Phase3
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      filename = File.join("views",
+                           self.class.to_s.underscore,
+                           "#{template_name}.html.erb")
+
+      template = File.read(filename)
+      content = ERB.new(template).result(binding)
+
+      render_content(content, 'text/html')
     end
   end
 end
